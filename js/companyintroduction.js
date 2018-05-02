b2e45f43fb78b74e62dcc4468916b054685a1e42
wujinli <wujinli>
2018-05-02 17:12:40 +0800

    页面初创

---
+++ js/companyintroduction.js
@@ -0,0 +1,12 @@
+function companyin() {
+    document.getElementById("content").src = "companyintroduction.html";
+}
+
+function companyqu() {
+    document.getElementById("content").src = "companyqualification.html";
+}
+
+function companyapp() {
+    document.getElementById("content").src = "companyappearance.html";
+}
+
